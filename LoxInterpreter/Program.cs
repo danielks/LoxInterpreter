c2b3e17e378ce5040d9f55a468c4aa1f749ac9ab
Daniel K. Silva <danielks@gmail.com>
2024-06-19 10:00:18 -0300

    Adicionar arquivos de projeto.

---
+++ LoxInterpreter/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LoxInterpreter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 1)
+            {
+                Console.WriteLine("Usage: LoxInterpreter [script]");
+
+                Environment.ExitCode = 64;
+
+            }
+            else if (args.Length == 1)
+            {
+                RunFile(args[0]);
+
+            }
+            else
+            {
+                RunPrompt();
+            }
+        }
+
+        private static void RunPrompt()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void RunFile(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
